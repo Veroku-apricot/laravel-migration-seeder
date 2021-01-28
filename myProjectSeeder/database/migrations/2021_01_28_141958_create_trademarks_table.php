d89b57883ad12c89841855890eb084bbeee36bd3
Veronika <veronikakuchtova95@gmail.com>
2021-01-28 15:41:24 +0100

    Migrations

---
+++ myProjectSeeder/database/migrations/2021_01_28_141958_create_trademarks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrademarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trademarks', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 100);
+            $table->text('logo');
+            $table->integer('number_of_locations') -> unsigned();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trademarks');
+    }
+}
